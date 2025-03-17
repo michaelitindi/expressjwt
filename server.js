c1448a3e67d4b3e973f11e7c4422d964b7ebbdb0
michaelitindi <michaelitindi@gmail.com>
2025-03-17 19:26:03 +0300

    created submitinterview endpoint

--- server.js
+++ server.js
@@ -8,15 +8,40 @@ if (process.env.NODE_ENV != "production"){
 
 // import dependancies
 const express = require("express");
-const connectToDb  = require("./config/connectToDb")
+const connectToDb  = require("./config/connectToDb");
+const Interview  = require("./models/interview");
 
 // crete an express app
 const app = express();
+
+//configure express app 
+app.use(express.json());
 
 // connect to database
 connectToDb();
 
 // Routing
+app.get("/", (req, res) => {
+    res.send("App is running...")
+});
+
+app.post("/submitinterview", async (req, res) => {
+//get data of off req body
+const title = req.body.title;
+const description = req.body.description;
+const questions = req.body.questions;
+
+//create interview
+const interview = await Interview.create({
+    title: title,
+    description: description,
+    questions: questions,
+});
+
+//respond with interview
+res.json({ interview: interview});
+
+});
 
 //start our server
 app.listen(process.env.PORT);
